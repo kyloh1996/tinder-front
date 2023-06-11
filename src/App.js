b2f3c365f0bc4068fc384e24ba034437926d14f9
kyloh1996 <kyloh1996@gmail.com>
2023-06-10 22:40:56 -0500

    show complete

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import {Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import PracticeIndex from './pages/PracticeIndex';
 import PracticeShow from './pages/PracticeShow';
+import PracticeNew from './pages/PracticeNew';
+
 import MockData from './MockData';
 
 
 import './App.css';
 
 function App() {
-  // using variables needs a useState for functionality 
-  const [practice,setPractice] = useState(MockData)
+// using variables needs a useState for functionality
+  // eslint-disable-next-line no-unused-vars
+    const [practice,setPractice] = useState(MockData)
 console.log(practice); // to see the data sent to server
 
   return (
@@ -21,10 +24,11 @@ console.log(practice); // to see the data sent to server
       <Header />
       <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/practiceindex" element={<PracticeIndex practice={practice}/>} />
+      <Route path="/practiceindex" element={<PracticeIndex practice={practice} />} />
       <Route path="/practiceshow/:id" element={<PracticeShow practice={practice} />} />
+      <Route path="/practicenew" element={<PracticeNew practice={practice} />} />
 
-      </Routes>
+    </Routes>
       <Footer />
     </div>
   );
